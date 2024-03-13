64fc7664087bbc1830f3b493c94be46fe53ba415
simoneaucello <simoneaucello@gmail.com>
2024-03-13 17:31:48 +0100

    [add] comments to js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const { createApp } = Vue;
 
+
 createApp({
-  data(){
-    return{
+
+  data() {
+    return {
+ 
       todoList: [
         {
           text: 'Finire esercitazione',
-          done: true
+          done: true // Indica se l'attività è stata completata
         },
         {
           text: 'Studiare Vue',
@@ -17,27 +20,37 @@ createApp({
           done: false
         },
       ],
+      // Variabile per memorizzare il testo della nuova attività
       newTask: '',
+      // Messaggio di errore da visualizzare
       errorMsg: ''
     }
   },
 
+
   methods: {
-    addTask(){
-      this.errorMsg = ''
-      const newText = this.newTask
-      if(newText.length >= 4){
-        this.todoList.unshift({text: newText, done: false })
-      }else{
+    // Metodo per aggiungere una nuova attività alla lista
+    addTask() {
+
+      this.errorMsg = '';
+      // Memorizza il testo della nuova attività
+      const newText = this.newTask;
+      // Verifica se il testo della nuova attività ha almeno 4 caratteri
+      if (newText.length >= 4) {
+        // Aggiunge la nuova attività all'inizio della lista
+        this.todoList.unshift({ text: newText, done: false });
+      } else {
+        // Mostra un messaggio di errore se il testo è troppo breve
         this.errorMsg = 'Attenzione! Il testo deve avere almeno 4 caratteri';
-      }     
-      this.newTask = ''     
+      }
+      // Resetta il campo di input per la nuova attività
+      this.newTask = '';
     },
 
-    deleteTask(indice){
-      this.todoList.splice(indice, 1)
-    } 
+    // Metodo per eliminare un'attività dalla lista
+    deleteTask(index) {
+      // Rimuove l'attività corrispondente all'indice specificato
+      this.todoList.splice(index, 1);
+    }
   },
-
-
-}).mount('#app')
+}).mount('#app'); 
